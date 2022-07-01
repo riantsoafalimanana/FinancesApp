0d06b618d0164dc8c54739f3fdcf2b8f9d770d5b
riantsoa <riantsoafalimanana@gmail.com>
2022-07-01 18:52:22 +0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import React from "react";
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from "@react-navigation/stack";
+import TouchScreen from "./src/screens/TouchScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    const AppStack = createStackNavigator();
+    const {Navigator, Screen} = AppStack;
+
+    return (
+        <NavigationContainer>
+            <Navigator headerMode='none'>
+                <Screen name='Touch' component={TouchScreen} />
+            </Navigator>
+        </NavigationContainer>
+    );
+}
---
+++ src/screens/TouchScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, Text, StyleSheet} from "react-native";
+import styled from 'styled-components';
+import {Fontisto, MaterialIcons} from '@expo/vector-icons';
+
+const TouchScreen = () => {
+    return (
+        <Container>
+
+        </Container>
+    );
+};
+
+const Container = styled.SafeAreaView`
+    flex: 1;
+    background-color: #1e1e1e;
+`;
+
+export default TouchScreen;
